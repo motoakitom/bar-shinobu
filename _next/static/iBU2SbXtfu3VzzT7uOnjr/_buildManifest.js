a18a19f7fa2ed71cc459c0728358facda5140fb5
motoakitom <motoakitom@users.noreply.github.com>
2026-02-12 03:28:40 +0000

    deploy: motoakitom/bar-shinobu-source@117674ccc70bf130ba00a59e5644c514a9132c89

--- _next/static/iBU2SbXtfu3VzzT7uOnjr/_buildManifest.js
+++ _next/static/iBU2SbXtfu3VzzT7uOnjr/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/bar-shinobu//_next/:path+",
+        "destination": "/bar-shinobu/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
